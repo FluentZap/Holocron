58d2173b57142a69bcb490f0ae1326cb7fb51ab1
Student <example@example.com>
2019-07-22 16:14:47 -0700

    add content for Login

--- ClientApp/src/components/login/Login.jsx
+++ ClientApp/src/components/login/Login.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Button } from '@material-ui/core'
 import { connect } from 'react-redux'
+import TextField from '@material-ui/core/TextField';
 import uuid from 'uuid';
+import { styled } from '@material-ui/styles';
 
 const appStyles = makeStyles({
   root: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
     // backgroundColor: '#0a0a12',
     '& h1': {
       fontFamily: 'Engli-Besh, Times, serif',
@@ -12,6 +19,21 @@ const appStyles = makeStyles({
       //fontFamily: 'Symbol, Times, serif',      
     }
   },
+  loginContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 20,
+    backgroundColor: '#334',
+    borderRadius: 10,
+  },
+})
+
+const StyledInput = styled(TextField)({
+  color: 'white',
+  margin: '15px 15px',
+  '& label, input': {
+    color: 'white'
+  }
 })
 
 
@@ -20,10 +42,24 @@ function Login(props) {
   const classes = appStyles();
   return (
     <div className={classes.root}>
-      {props.messages.map(m => (
+      <div className={classes.loginContainer}>
+        <StyledInput
+          label="Username"
+          className={classes.textField}
+        />
+        <StyledInput
+          type="password"
+          label="Password"
+          className={classes.textField}
+        />
+        <Button variant="contained" color="primary">
+        Login
+        </Button>
+        {/* {props.messages.map(m => (
         <h1 key={uuid.v4()}>{m}</h1>
       )
-      )}
+      )} */}
+      </div>
     </div>
   );
 }
