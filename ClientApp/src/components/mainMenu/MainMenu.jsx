f2f357b1dc0dd1d27fe7f943e62d5fc351422e20
Student <example@example.com>
2019-07-29 13:13:25 -0700

    add menu layout

--- ClientApp/src/components/mainMenu/MainMenu.jsx
+++ ClientApp/src/components/mainMenu/MainMenu.jsx
@@ -6,53 +6,32 @@ import uuid from 'uuid';
 import { styled } from '@material-ui/styles';
 import './mainMenuStyles.css';
 
-const appStyles = makeStyles({
-  root: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    // backgroundColor: '#0a0a12',
-    '& h1': {
-      fontFamily: 'Engli-Besh, Times, serif',
-      // fontFamily: 'Teuton',
-      //fontFamily: 'Symbol, Times, serif',      
-    }
-  },
-  loginContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: 20,
-    backgroundColor: '#334',
-    borderRadius: 10,
-  },
-})
-
-const StyledButton = styled(TextField)({
-  color: 'white',
-  margin: '15px 15px',
-  '& label, input': {
-    color: 'white'
-  }
-})
-
-
-
 function MainMenu(props) {
-  const classes = appStyles();
   return (
     <div className='flex-center full-screen'>
       <div className='main-menu-root'>
-        <svg viewBox="0 0 0.5625 1" />
+        <svg viewBox="0 0 0.5625 1">
+          <defs>
+            {/* <clipPath id="myClip">
+              <circle cx="100" cy="100" r="120" />
+              <circle cx="60" cy="60" r="40" />
+            </clipPath> */}
+          </defs>
+        </svg>
+
+
         <div className='main-menu-container'>
-          {/* <Button variant="contained" color="primary" >Games</Button> */}
-          {/* <Button variant="contained" color="secondary" >Characters</Button> */}
-          {/* <Button variant="contained" color="primary" >Data Archives</Button>       */}
-          {/* {props.messages.map(m => (
-        <h1 key={uuid.v4()}>{m}</h1>
-      )
-      )} */}
+        <button className='flex-center login-button' style={{ gridArea: '2 / 1 / span 6 / span 4', margin: 5}}>Roster</button>
+        
+        <button className='flex-center login-button' style={{ gridArea: '2 / 5 / span 3 / span 5', margin: 5}}>Archives</button>
+        <button className='flex-center login-button' style={{ gridArea: '5 / 5 / span 3 / span 5', margin: 5}}>Hanger</button>
+        
+        <button className='flex-center login-button' style={{ gridArea: '8 / 1 / span 3 / span 9', margin: 5}}>Adventures</button>
+        
+        <button className='flex-center login-button' style={{ gridArea: '11 / 1 / span 3 / span 5', margin: 5}}>Atlas</button>
+        <button className='flex-center login-button' style={{ gridArea: '14 / 1 / span 3 / span 5', margin: 5}}>Notes</button>
+        
+        <button className='flex-center login-button' style={{ gridArea: '11 / 6 / span 6 / span 4', margin: 5}}>Dice Roller</button>
         </div>
       </div>
     </div>
