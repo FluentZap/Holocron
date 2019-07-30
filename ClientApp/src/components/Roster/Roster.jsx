6b4ac3ab9df6c02da054365afd23c921f6c70790
Todd Aden <FluentZap@Gmail.com>
2019-07-29 21:38:17 -0700

    add BG mask and color

--- ClientApp/src/components/Roster/Roster.jsx
+++ ClientApp/src/components/Roster/Roster.jsx
@@ -6,68 +6,13 @@ import './RosterStyles.css';
 function Roster(props) {
   return (
     <div className='flex-center full-screen'>
-      {/* <div className='roster-root'> */}
-        {/* <svg viewBox="0 0 0.5625 1"> */}
-          {/* <defs> */}
-            {/* <clipPath id="myClip">
-              <circle cx="100" cy="100" r="120" />
-              <circle cx="60" cy="60" r="40" />
-            </clipPath> */}
-          {/* </defs> */}
-        {/* </svg> */}
-
-
-        <div className='roster-container'>
-          <button onClick={() => navigate('/menu')} className='flex-center flat-button scanlines' style={{ gridArea: '1 / 1 / span 1 / span 4', margin: 5 }}>Menu</button>
-          <div className='flex-center flat-container roster-character-list'>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-            <div className='flex-center flat-button roster-detail-box scanlines'/>
-          </div>
+      <div className='roster-container'>
+        <button onClick={() => navigate('/menu')} className='flex-center data-panel red-glow scanlines' style={{ gridArea: '1 / 1 / span 1 / span 4', margin: 5 }}>Menu</button>
+        <div className='flex-center data-panel gray-flat roster-character-list'>
+          <div className='flex-center data-panel red-glow roster-detail-box scanlines' />
+          <div className='flex-center data-panel red-glow roster-detail-box scanlines' />
         </div>
-      {/* </div> */}
+      </div>      
     </div>
   );
 }
