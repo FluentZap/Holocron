43c58fe52938c8eeede4319ebf61273312e4b673
Student <example@example.com>
2019-07-29 17:21:01 -0700

    set layout to scale off vh

--- ClientApp/src/components/Roster/Roster.jsx
+++ ClientApp/src/components/Roster/Roster.jsx
@@ -6,68 +6,68 @@ import './RosterStyles.css';
 function Roster(props) {
   return (
     <div className='flex-center full-screen'>
-      <div className='main-menu-root'>
-        <svg viewBox="0 0 0.5625 1">
-          <defs>
+      {/* <div className='roster-root'> */}
+        {/* <svg viewBox="0 0 0.5625 1"> */}
+          {/* <defs> */}
             {/* <clipPath id="myClip">
               <circle cx="100" cy="100" r="120" />
               <circle cx="60" cy="60" r="40" />
             </clipPath> */}
-          </defs>
-        </svg>
+          {/* </defs> */}
+        {/* </svg> */}
 
 
-        <div className='main-menu-container'>
-          <button onClick={() => navigate('/roster')} className='flex-center flat-button scanlines' style={{ gridArea: '1 / 1 / span 1 / span 4', margin: 5, fontSize: 20 }}>Menu</button>
-          <div className='flex-center flat-container scanlines roster-character-list'>
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+        <div className='roster-container'>
+          <button onClick={() => navigate('/menu')} className='flex-center flat-button scanlines' style={{ gridArea: '1 / 1 / span 1 / span 4', margin: 5 }}>Menu</button>
+          <div className='flex-center flat-container roster-character-list'>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
 
-            <div className='flex-center flat-button roster-detail-box'/>
-            <div className='flex-center flat-button roster-detail-box'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
+            <div className='flex-center flat-button roster-detail-box scanlines'/>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
--- ClientApp/src/components/mainMenu/MainMenu.jsx
+++ ClientApp/src/components/mainMenu/MainMenu.jsx
@@ -6,30 +6,19 @@ import './MainMenuStyles.css';
 function MainMenu(props) {
   return (
     <div className='flex-center full-screen'>
-      <div className='main-menu-root'>
-        <svg viewBox="0 0 0.5625 1">
-          <defs>
-            {/* <clipPath id="myClip">
-              <circle cx="100" cy="100" r="120" />
-              <circle cx="60" cy="60" r="40" />
-            </clipPath> */}
-          </defs>
-        </svg>
 
+      <div className='datapad-container'>
+        <button onClick={() => navigate('/roster')} className='flex-center flat-button scanlines' style={{ gridArea: '2 / 1 / span 6 / span 4', margin: 5 }}>Roster</button>
 
-        <div className='main-menu-container'>
-        <button onClick={() => navigate('/roster')} className='flex-center flat-button scanlines' style={{ gridArea: '2 / 1 / span 6 / span 4', margin: 5}}>Roster</button>
-        
-        <button onClick={() => navigate('/archives')} className='flex-center flat-button scanlines' style={{ gridArea: '2 / 5 / span 3 / span 5', margin: 5}}>Archives</button>
-        <button onClick={() => navigate('/hanger')} className='flex-center flat-button scanlines' style={{ gridArea: '5 / 5 / span 3 / span 5', margin: 5}}>Hanger</button>
-        
-        <button onClick={() => navigate('/adventures')} className='flex-center flat-button scanlines' style={{ gridArea: '8 / 1 / span 3 / span 9', margin: 5}}>Adventures</button>
-        
-        <button onClick={() => navigate('/atlas')} className='flex-center flat-button scanlines' style={{ gridArea: '11 / 1 / span 3 / span 5', margin: 5}}>Atlas</button>
-        <button onClick={() => navigate('/notes')} className='flex-center flat-button scanlines' style={{ gridArea: '14 / 1 / span 3 / span 5', margin: 5}}>Notes</button>
-        
-        <button onClick={() => navigate('/roller')} className='flex-center flat-button scanlines' style={{ gridArea: '11 / 6 / span 6 / span 4', margin: 5}}>Dice Roller</button>
-        </div>
+        <button onClick={() => navigate('/archives')} className='flex-center flat-button scanlines' style={{ gridArea: '2 / 5 / span 3 / span 5', margin: 5 }}>Archives</button>
+        <button onClick={() => navigate('/hanger')} className='flex-center flat-button scanlines' style={{ gridArea: '5 / 5 / span 3 / span 5', margin: 5 }}>Hanger</button>
+
+        <button onClick={() => navigate('/adventures')} className='flex-center flat-button scanlines' style={{ gridArea: '8 / 1 / span 3 / span 9', margin: 5 }}>Adventures</button>
+
+        <button onClick={() => navigate('/atlas')} className='flex-center flat-button scanlines' style={{ gridArea: '11 / 1 / span 3 / span 5', margin: 5 }}>Atlas</button>
+        <button onClick={() => navigate('/notes')} className='flex-center flat-button scanlines' style={{ gridArea: '14 / 1 / span 3 / span 5', margin: 5 }}>Notes</button>
+
+        <button onClick={() => navigate('/roller')} className='flex-center flat-button scanlines' style={{ gridArea: '11 / 6 / span 6 / span 4', margin: 5 }}>Dice Roller</button>
       </div>
     </div>
   );
