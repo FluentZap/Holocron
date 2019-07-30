08d842c1e12f40706da50ef6da6e9584885b8388
Todd Aden <FluentZap@Gmail.com>
2019-07-29 21:52:10 -0700

    update border to white

--- ClientApp/src/components/Roster/Roster.jsx
+++ ClientApp/src/components/Roster/Roster.jsx
@@ -6,7 +6,7 @@ import './RosterStyles.css';
 function Roster(props) {
   return (
     <div className='flex-center full-screen'>
-      <div className='roster-container'>
+      <div className='data-container'>
         <button onClick={() => navigate('/menu')} className='flex-center data-panel red-glow scanlines' style={{ gridArea: '1 / 1 / span 1 / span 4', margin: 5 }}>Menu</button>
         <div className='flex-center data-panel gray-flat roster-character-list'>
           <div className='flex-center data-panel red-glow roster-detail-box scanlines' />
