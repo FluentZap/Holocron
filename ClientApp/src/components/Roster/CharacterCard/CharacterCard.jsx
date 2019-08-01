0178d7b7399a1720acb4d0feaf2ca8e79e3ab616
Todd Aden <FluentZap@Gmail.com>
2019-08-01 00:32:52 -0700

    add skills

--- ClientApp/src/components/Roster/CharacterCard/CharacterCard.jsx
+++ ClientApp/src/components/Roster/CharacterCard/CharacterCard.jsx
@@ -5,54 +5,52 @@ import FadeInBuilder from '../../FadeInBuilder';
 
 const boxFade = new FadeInBuilder(0.3, 0.5, 8);
 
+export const TextBox = ({ text, area, delay, className }) => {
+    if (!className) className = '';
+    return <div className={'font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text ' + className}
+        style={{ gridArea: `${area[0]} / ${area[1]} / span ${area[2]} / span ${area[3]}`, animationDelay: delay }}>
+        {text}</div>
+}
+
+
+
 const CharacterCard = ({ fadeDelay, stats }) => {
     return (
-        <div className='animate-fade-in roster-character-card' style={{ gridArea: '3 / 1 / span 10 / span 20', animationDelay: fadeDelay }}>
+        <div className='animate-fade-in roster-character-card' style={{ gridArea: '4 / 1 / span 10 / span 20', animationDelay: fadeDelay }}>
             <div className='data-panel red-flat scanlines-back m2 p2' style={{ gridArea: '1 / 1 / span 10 / span 20' }} />
+            <TextBox area={[2, 7, 2, 13]} delay={boxFade()}
+                text={`Name: ${stats.name}`} />
 
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '2 / 7 / span 2 / span 13', animationDelay: boxFade() }}>
-                Name: {stats ? stats.name : ''}</div>
+            <TextBox area={[4, 7, 2, 8]} delay={boxFade()}
+                text={`Species: ${stats.species}`} />
 
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '4 / 7 / span 2 / span 8', animationDelay: boxFade() }}>
-                Species: {stats.species}
-            </div>
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '4 / 15 / span 2 / span 5', animationDelay: boxFade() }}>
-                {stats.career}
-            </div>
+            <TextBox area={[4, 15, 2, 5]} delay={boxFade()}
+                text={stats.career} />
 
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '6 / 7 / span 4 / span 4', animationDelay: boxFade() }}>
-                <div>Wounds</div>
-                <div>{stats.wound} / {stats.woundThreshold}</div>
-            </div>
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '6 / 11 / span 4 / span 4', animationDelay: boxFade() }}>
-                <div>Strain</div>
-                <div>{stats.strain} / {stats.strainThreshold}</div>
-            </div>
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '6 / 15 / span 4 / span 5', animationDelay: boxFade() }}>
-                {stats.specializations.map(spec => <div key={uuid.v4()}>{spec}</div>)}
-            </div>
+            <TextBox area={[6, 7, 4, 4]} delay={boxFade()}
+                text={<>
+                    <div>Wounds</div>
+                    <div>{stats.wound} / {stats.woundThreshold}</div>
+                </>} />
+            <TextBox area={[6, 11, 4, 4]} delay={boxFade()}
+                text={<>
+                    <div>Strain</div>
+                    <div>{stats.strain} / {stats.strainThreshold}</div>
+                </>} />
+            <TextBox area={[6, 15, 4, 5]} delay={boxFade()}
+                text={stats.specializations.map(spec => <div key={uuid.v4()}>{spec}</div>)} />
 
+            <TextBox area={[2, 2, 5, 5]} delay={boxFade()}
+                text={''} />
 
-            <div className='font-small animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '2 / 2 / span 5 / span 5', animationDelay: boxFade() }}>
-                {/* PIC */}
-            </div>
-            <div className='font-small animate-fade-in z-5 m2 p1 flex-left data-panel gray-flat scanlines-back roster-text'
-                style={{ gridArea: '7 / 2 / span 3 / span 5', animationDelay: boxFade() }}>
-                <div>xp {stats.xp}</div>
-                <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <div className='credits' />
-                    {stats.credits}
-                </div>
-            </div>
-
-
+            <TextBox area={[7, 2, 3, 5]} delay={boxFade()}
+                text={<>
+                    <div>xp {stats.xp}</div>
+                    <div style={{ flexDirection: 'row', display: 'flex' }}>
+                        <div className='credits' />
+                        {stats.credits}
+                    </div>
+                </>} />
         </div>
     );
 }
--- ClientApp/src/components/Roster/RosterCreateNew/RosterCreateNew.jsx
+++ ClientApp/src/components/Roster/RosterCreateNew/RosterCreateNew.jsx
@@ -4,11 +4,12 @@ import { navigate } from "@reach/router";
 import uuid from 'uuid';
 import './RosterCreateNewStyles.css';
 import FadeInBuilder from '../../FadeInBuilder';
-import CharacterCard from '../CharacterCard/CharacterCard';
+import CharacterCard, { TextBox } from '../CharacterCard/CharacterCard';
 
 
-const fadeIn = new FadeInBuilder(0, 0.3, 6);
+const fadeIn = new FadeInBuilder(0, 0.3, 4);
 const fadeStats = new FadeInBuilder(0.3, 0.5, 6);
+const boxFade = new FadeInBuilder(0.3, 0.5, 8);
 
 function Roster(props) {
   const { character } = props;
@@ -25,50 +26,74 @@ function Roster(props) {
     woundThreshold: 10,
     strain: 0,
     strainThreshold: 10,
+    soak: 2,
     brawn: 3,
     agility: 1,
     intellect: 2,
     cunning: 2,
     willpower: 2,
     presence: 2,
+    skills: {
+      astrogation: 0,
+      athletics: 0,
+      brawl: 0,
+      charm: 0,
+      coercion: 0,
+      computers: 0,
+      cool: 0,
+      coordination: 0,
+      coreWorlds: 0,
+      deception: 0,
+      discipline: 0,
+      gunnery: 0,
+      leadership: 0,
+      lightsaber: 0,
+      lore: 0,
+      mechanics: 0,
+      medicine: 0,
+      melee: 0,
+      negotiation: 0,
+      outerRim: 0,
+      perception: 0,
+      pilotingPlanetary: 0,
+      pilotingSpace: 0,
+      rangedHeavy: 0,
+      rangedLight: 0,
+      resilience: 0,
+      skulduggery: 0,
+      stealth: 0,
+      streatwise: 0,
+      survival: 0,
+      underworld: 0,
+      vigilance: 0,
+      xenology: 0,
+      warfare: 0,
+      cybernetics: 0,
+    }
   });
 
   const [selectStat, setSelectStat] = useState('')
 
-  const StatBox = ({ name, value }) => {
-    let classes = 'roster-new-characteristics-box flex-center data-panel scanlines-back';
-
-    return name === selectStat ?
-      // return 1 === 1 ?
-      <div onClick={() => { setSelectStat(name) }} className={classes + ' orange-glow roster-detail-selected'}>
-        {/* <div className={'roster-new-characteristics-details'}> */}
-        <div style={{ fontFamily: 'Engli-Besh' }}>{value}</div>
-        {name}        
-        {/* </div> */}
-        <div className={'roster-new-characteristics-details'}>
-          <div className={'flex-center data-panel orange-glow scanlines-back roster-new-characteristics-modifier-box'}>Stuff</div>
-          <div className={'flex-center data-panel orange-glow scanlines-back roster-new-characteristics-modifier-box'}>Stuff</div>
-        </div>
-      </div>
-      :
-      <div onClick={() => { setSelectStat(name) }} className={classes + ' blue-glow '} style={{ animationDelay: fadeStats() }}>
-        <div style={{ fontFamily: 'Engli-Besh' }}>{value}</div>
-        {name}        
-      </div>
+  const StatBox = ({ name, value, selected }) => {
+    let classes = 'roster-new-characteristics-box flex-center data-panel scanlines-back'
+      + ((name === selectStat) ? ' orange-glow roster-detail-selected' : ' blue-glow ');
+    return <div onClick={() => { setSelectStat(name) }} className={classes} >
+      <div style={{ fontFamily: 'Engli-Besh' }}>{value}</div>
+      {name}
+    </div>
   }
-
 
   return (
     <div className='flex-center full-screen'>
       <div className='data-container'>
         <button onClick={() => navigate('/menu')} className='font-small animate-fade-in flex-center data-panel red-glow scanlines-back m2'
-          style={{ gridArea: '1 / 1 / span 2 / span 7', animationDelay: fadeIn() }}>Menu</button>
+          style={{ gridArea: '1 / 1 / span 3 / span 7', animationDelay: fadeIn() }}>Menu</button>
         {/* <div className='animate-fade-in flex-center data-panel red-glow scanlines' style={{ gridArea: '2 / 1 / span 3 / span 9', margin: 5, animationDelay: fadeIn() }}> */}
         <CharacterCard fadeDelay={fadeIn()} stats={stats} />
         {/* </div> */}
 
 
-        <div className='roster-new-characteristics-list animate-fade-in flex-center data-panel red-flat scanlines-back m2' style={{ gridArea: '13 / 1 / span 28 / span 7', animationDelay: fadeIn() }}>
+        <div className='roster-new-characteristics-list animate-fade-in flex-center data-panel red-flat scanlines-back m2' style={{ gridArea: '14 / 1 / span 24 / span 7', animationDelay: fadeIn() }}>
           <StatBox name={'Brawn'} value={stats.brawn} />
           <StatBox name={'Agility'} value={stats.agility} />
           <StatBox name={'Intellect'} value={stats.intellect} />
@@ -77,10 +102,21 @@ function Roster(props) {
           <StatBox name={'Presence'} value={stats.presence} />
         </div>
 
+        {/* <div className='animate-fade-in roster-skills-card' style={{ gridArea: '14 / 8 / span 24 / span 13', animationDelay: fadeIn() }}> */}
 
-        <div className='animate-fade-in flex-center data-panel red-glow scanlines-back m2' style={{ gridArea: '13 / 8 / span 8 / span 13', animationDelay: fadeIn() }} />
-        <div className='animate-fade-in flex-center data-panel red-glow scanlines-back m2' style={{ gridArea: '21 / 8 / span 8 / span 13', animationDelay: fadeIn() }} />
-        <div className='animate-fade-in flex-center data-panel red-glow scanlines-back m2' style={{ gridArea: '29 / 8 / span 8 / span 13', animationDelay: fadeIn() }} />
+          <div className='animate-fade-in flex-center data-panel red-flat scanlines-back m2' style={{ gridArea: '14 / 8 / span 24 / span 13', display: 'block', overflow: 'auto' }} >
+          {Object.entries(stats.skills).map((value, i) => {
+            return <div className='animate-fade-in z-5 m2 p2 flex-left data-panel gray-flat roster-text'
+              style={{animationDelay: boxFade() }}>
+              {value[0]}: {value[1]}
+            </div>
+          })
+
+          }
+          {/* <TextBox area={[2, 2, 2, 11]} delay={boxFade()} className={'m2'}
+            text={'stuff'} /> */}
+
+        </div>
       </div>
     </div>
   );
