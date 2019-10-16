6a1faddb55291c529b7bc2a9f51e62fc7ab54d18
Todd Aden <FluentZap@Gmail.com>
2019-10-16 10:49:57 -0700

    update login styles

--- ClientApp/src/components/Login/Login.jsx
+++ ClientApp/src/components/Login/Login.jsx
@@ -21,23 +21,23 @@ function Login(props) {
   return (
     <div className='flex-center login-root'>
       <div className='login-container'>
-        <div className='flex-center login-heading'>HoloCron</div>
-        <input className='login-input' style={{ gridArea: '4 / 2 / span 2 / span 10' }} placeholder={'Username'} type="text" onChange={event => setUserName(event.target.value)} />
+        <div className='flex-center login-heading' style={{ gridArea: '11 / 4 / span 3 / span 14' }} >HoloCron</div>
+        <input className='login-input m2 p2' style={{ gridArea: '16 / 4 / span 3 / span 14'}} placeholder={'Username'} type="text" onChange={event => setUserName(event.target.value)} />
         <input onKeyUp={event => {
           if (event.key === 'Enter') {
             LoginUser();
           }
         }
-        } className='login-input' style={{ gridArea: '7 / 2 / span 2 / span 10' }} placeholder={'Password'} type="password" onChange={event => setPassword(event.target.value)} />
+        } className='login-input m2 p2' style={{ gridArea: '20 / 4 / span 3 / span 14' }} placeholder={'Password'} type="password" onChange={event => setPassword(event.target.value)} />
         {userName === '' || password === '' ?
           <>
-            <button className='flex-center data-panel disabled' style={{ gridArea: '10 / 2 / span 2 / span 4', fontSize: '4vh' }}>Login</button>
-            <button className='flex-center data-panel disabled' style={{ gridArea: '10 / 8 / span 2 / span 4', fontSize: '4vh' }}>Register</button>
+            <button className='flex-center data-panel disabled m2 p2' style={{ gridArea: '27 / 2 / span 4 / span 9', fontSize: '4vh' }}>Login</button>
+            <button className='flex-center data-panel disabled m2 p2' style={{ gridArea: '27 / 11 / span 4 / span 9', fontSize: '4vh' }}>Register</button>
           </>
           :
           <>
-            <button onClick={LoginUser} className='flex-center data-panel red-glow' style={{ gridArea: '10 / 2 / span 2 / span 4', fontSize: '4vh' }}>Login</button>
-            <button onClick={AddUser} className='flex-center data-panel red-glow' style={{ gridArea: '10 / 8 / span 2 / span 4', fontSize: '4vh' }}>Register</button>
+            <button onClick={LoginUser} className='flex-center data-panel red-glow m2 p2' style={{ gridArea: '27 / 2 / span 4 / span 9', fontSize: '4vh' }}>Login</button>
+            <button onClick={AddUser} className='flex-center data-panel red-glow m2 p2' style={{ gridArea: '27 / 11 / span 4 / span 9', fontSize: '4vh' }}>Register</button>
           </>
         }
       </div>
