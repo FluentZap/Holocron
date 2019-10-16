9e45c9d00d7196555335e93e3af7de85ac2765b7
Todd Aden <FluentZap@Gmail.com>
2019-10-16 11:43:10 -0700

    update screenshots

--- ClientApp/src/components/Login/Login.jsx
+++ ClientApp/src/components/Login/Login.jsx
@@ -7,10 +7,10 @@ import './LoginStyles.css';
 function Login(props) {
   const { dispatch } = props;
   const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');  
-  
+  const [password, setPassword] = useState('');
 
-  const AddUser = () => {    
+
+  const AddUser = () => {
     dispatch({ type: 'SERVER_CREATE_USER', userName: userName, password: password });
   }
 
@@ -21,14 +21,15 @@ function Login(props) {
   return (
     <div className='flex-center login-root'>
       <div className='login-container'>
+        <div className='flex-center login-background' style={{ gridArea: '9 / 1 / span 24 / span 20' }} />
         <div className='flex-center login-heading' style={{ gridArea: '11 / 4 / span 3 / span 14' }} >HoloCron</div>
-        <input className='login-input m2 p2' style={{ gridArea: '16 / 4 / span 3 / span 14'}} placeholder={'Username'} type="text" onChange={event => setUserName(event.target.value)} />
+        <input className='login-input m2 p2' style={{ gridArea: '16 / 4 / span 4 / span 14' }} placeholder={'Username'} type="text" onChange={event => setUserName(event.target.value)} />
         <input onKeyUp={event => {
           if (event.key === 'Enter') {
             LoginUser();
           }
         }
-        } className='login-input m2 p2' style={{ gridArea: '20 / 4 / span 3 / span 14' }} placeholder={'Password'} type="password" onChange={event => setPassword(event.target.value)} />
+        } className='login-input m2 p2' style={{ gridArea: '20 / 4 / span 4 / span 14' }} placeholder={'Password'} type="password" onChange={event => setPassword(event.target.value)} />
         {userName === '' || password === '' ?
           <>
             <button className='flex-center data-panel disabled m2 p2' style={{ gridArea: '27 / 2 / span 4 / span 9', fontSize: '4vh' }}>Login</button>
